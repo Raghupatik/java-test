03f9bb46c8b24b6eda6c42dbbc5c7b0fa117492e
PavaniKon <134952151+PavaniKon@users.noreply.github.com>
2023-06-27 18:22:39 -0500

    Update GreetingController.java

--- src/main/java/com/example/sample/GreetingController.java
+++ src/main/java/com/example/sample/GreetingController.java
@@ -2,7 +2,7 @@ package com.example.sample;
 
 
 public class GreetingController {
-    public String greeting() {
+    public String Function2() {
         return "Lambda is Executed Successfully with Version 1.0.3 !!";
     }
 }
