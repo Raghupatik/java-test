9b90228a10828a63b105b814fb6f30a919945400
Raghupatik <ragupathi.kommidi@gmail.com>
2023-07-21 10:02:51 -0500

    1.0.4 changes

--- src/main/java/com/example/sample/GreetingController.java
+++ src/main/java/com/example/sample/GreetingController.java
@@ -3,15 +3,6 @@ package com.example.sample;
 
 public class GreetingController {
     public String greeting() {
-        const response = {
-                statusCode: 200,
-                headers: {
-                    'Content-Type': "application/json"
-                },
-                body: JSON.stringify({
-                    name: "Lambda is Executed Successfully with Version 1.0.3 !!"
-                })
-        };
-        return response;
+        return "Lambda is Executed Successfully with Version 1.0.4 !!"
     }
 }
